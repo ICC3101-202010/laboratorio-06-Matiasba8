3bffb730e5ea59aeb49410fc002bd6008b33c6d9
MatiBa8 <mibustos2@miuandes.cl>
2020-05-18 18:38:35 -0400

    End

---
+++ Laboratorio 6 MatíasBustos/Area.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_6_MatíasBustos
+{
+    [Serializable]
+    class Area : Division
+    {
+        public Area(string nombre, List<Persona> personas, string sector) : base(nombre, personas, sector)
+        {
+        }
+    }
+}
---
+++ Laboratorio 6 MatíasBustos/Division.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_6_MatíasBustos
+{
+    [Serializable]
+    class Division
+    {
+        private string Nombre;
+        private string SectorName;
+
+        private List<Persona> Personal = new List<Persona>();
+
+       
+
+        public Division(string nombre, List<Persona> personas,string sector)
+        {
+            Nombre1 = nombre;
+            Personal1 = personas;
+            SectorName1 = sector;
+        }
+
+        public string Nombre1 { get => Nombre; set => Nombre = value; }
+        public string SectorName1 { get => SectorName; set => SectorName = value; }
+        internal List<Persona> Personal1 { get => Personal; set => Personal = value; }
+    }
+}
